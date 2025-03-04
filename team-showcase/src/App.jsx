50846ad2c835e344d19c047c4e0e310933d495f6
THINK BOOK <dhairya.jangir.s73@kalvium.community>
2025-03-04 14:22:09 +0530

    done

---
+++ team-showcase/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import TeamMemberCard from './components/TeamMemberCard'
+
+function App() {
+  return (
+    <>
+      <div>
+        <TeamMemberCard />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ team-showcase/src/components/TeamMemberCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TeamMemberCard = () => {
+    return <div>
+        <h1>Sohan Shah, Software Engineer</h1>
+        <h1>Denis Devin, Dentist</h1>
+    </div>
+}
+
+export default TeamMemberCard;
